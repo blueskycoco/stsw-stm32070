f24e145c1c79859b025091570928c0050c4466c4
U-CORP\dillon.min <dillon.min@CNBNG-LT542.Corp.irdeto.com>
2015-06-25 11:27:48 +0800

    init version

---
+++ STM32F4x7_ETH_LwIP_V1.1.1/Project/Standalone/tcp_echo_client/src/serial_debug.c
@@ -0,0 +1,190 @@
+/**
+  ******************************************************************************
+  * @file    serial_debug.c
+  * @author  MCD Application Team
+  * @version V1.1.0
+  * @date    31-July-2013
+  * @brief   This file provide functions to retarget the C library printf function
+  *          to the USART. 
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2013 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx.h"
+
+#if defined (USE_STM324xG_EVAL)
+  //#include "stm324xg_eval.h"
+
+#elif defined (USE_STM324x7I_EVAL) 
+  //#include "stm324x7i_eval.h"
+
+#else
+ #error "Please select first the Evaluation board used in your application (in Project Options)"
+#endif
+
+#include "serial_debug.h"
+#include <stdio.h>
+
+#ifdef SERIAL_DEBUG
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+#ifdef __GNUC__
+  /* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
+     set to 'Yes') calls __io_putchar() */
+  #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+  #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Initialize COM1 interface for serial debug
+  * @note   COM1 interface is defined in stm3210g_eval.h file (under Utilities\STM32_EVAL\STM324xG_EVAL)  
+  * @param  None
+  * @retval None
+  */
+#define COMn                             1
+#define EVAL_COM1                        USART6
+#define EVAL_COM1_CLK                    RCC_APB2Periph_USART6
+#define EVAL_COM1_TX_PIN                 GPIO_Pin_6
+#define EVAL_COM1_TX_GPIO_PORT           GPIOC
+#define EVAL_COM1_TX_GPIO_CLK            RCC_AHB1Periph_GPIOC
+#define EVAL_COM1_TX_SOURCE              GPIO_PinSource6
+#define EVAL_COM1_TX_AF                  GPIO_AF_USART6
+#define EVAL_COM1_RX_PIN                 GPIO_Pin_7
+#define EVAL_COM1_RX_GPIO_PORT           GPIOC
+#define EVAL_COM1_RX_GPIO_CLK            RCC_AHB1Periph_GPIOC
+#define EVAL_COM1_RX_SOURCE              GPIO_PinSource7
+#define EVAL_COM1_RX_AF                  GPIO_AF_USART6
+#define EVAL_COM1_IRQn                   USART6_IRQn
+USART_TypeDef* COM_USART[COMn] = {EVAL_COM1}; 
+
+GPIO_TypeDef* COM_TX_PORT[COMn] = {EVAL_COM1_TX_GPIO_PORT};
+ 
+GPIO_TypeDef* COM_RX_PORT[COMn] = {EVAL_COM1_RX_GPIO_PORT};
+
+const uint32_t COM_USART_CLK[COMn] = {EVAL_COM1_CLK};
+
+const uint32_t COM_TX_PORT_CLK[COMn] = {EVAL_COM1_TX_GPIO_CLK};
+ 
+const uint32_t COM_RX_PORT_CLK[COMn] = {EVAL_COM1_RX_GPIO_CLK};
+
+const uint16_t COM_TX_PIN[COMn] = {EVAL_COM1_TX_PIN};
+
+const uint16_t COM_RX_PIN[COMn] = {EVAL_COM1_RX_PIN};
+ 
+const uint16_t COM_TX_PIN_SOURCE[COMn] = {EVAL_COM1_TX_SOURCE};
+
+const uint16_t COM_RX_PIN_SOURCE[COMn] = {EVAL_COM1_RX_SOURCE};
+ 
+const uint16_t COM_TX_AF[COMn] = {EVAL_COM1_TX_AF};
+ 
+const uint16_t COM_RX_AF[COMn] = {EVAL_COM1_RX_AF};
+ typedef enum 
+ {
+   COM1 = 0,
+   COM2 = 1
+ } COM_TypeDef;
+
+ void STM_EVAL_COMInit(COM_TypeDef COM, USART_InitTypeDef* USART_InitStruct)
+{
+  GPIO_InitTypeDef GPIO_InitStructure;
+
+  /* Enable GPIO clock */
+  RCC_AHB1PeriphClockCmd(COM_TX_PORT_CLK[COM] | COM_RX_PORT_CLK[COM], ENABLE);
+
+  if (COM == COM1)
+  {
+    /* Enable UART clock */
+    RCC_APB2PeriphClockCmd(COM_USART_CLK[COM], ENABLE);
+  }
+
+  /* Connect PXx to USARTx_Tx*/
+  GPIO_PinAFConfig(COM_TX_PORT[COM], COM_TX_PIN_SOURCE[COM], COM_TX_AF[COM]);
+
+  /* Connect PXx to USARTx_Rx*/
+  GPIO_PinAFConfig(COM_RX_PORT[COM], COM_RX_PIN_SOURCE[COM], COM_RX_AF[COM]);
+
+  /* Configure USART Tx as alternate function  */
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+
+  GPIO_InitStructure.GPIO_Pin = COM_TX_PIN[COM];
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(COM_TX_PORT[COM], &GPIO_InitStructure);
+
+  /* Configure USART Rx as alternate function  */
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_Pin = COM_RX_PIN[COM];
+  GPIO_Init(COM_RX_PORT[COM], &GPIO_InitStructure);
+
+  /* USART configuration */
+  USART_Init(COM_USART[COM], USART_InitStruct);
+    
+  /* Enable USART */
+  USART_Cmd(COM_USART[COM], ENABLE);
+}
+void DebugComPort_Init(void)
+{
+  USART_InitTypeDef USART_InitStructure;
+
+  /* USARTx configured as follow:
+        - BaudRate = 115200 baud  
+        - Word Length = 8 Bits
+        - One Stop Bit
+        - No parity
+        - Hardware flow control disabled (RTS and CTS signals)
+        - Receive and transmit enabled
+  */
+  USART_InitStructure.USART_BaudRate = 115200;
+  USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+  USART_InitStructure.USART_StopBits = USART_StopBits_1;
+  USART_InitStructure.USART_Parity = USART_Parity_No;
+  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+
+  STM_EVAL_COMInit(COM1, &USART_InitStructure);
+}
+
+/**
+  * @brief  Retargets the C library printf function to the USART.
+  * @param  None
+  * @retval None
+  */
+PUTCHAR_PROTOTYPE
+{
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the USART */
+  USART_SendData(EVAL_COM1, (uint8_t) ch);
+
+  /* Loop until the end of transmission */
+  while (USART_GetFlagStatus(EVAL_COM1, USART_FLAG_TC) == RESET)
+  {}
+
+  return ch;
+}
+#endif /* SERIAL_DEBUG */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
